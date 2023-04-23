a40466238f1a9a352ca6c1c8b2f94a150d2d6a04
vinodbaret <vinodsondliya@gmail.com>
2023-04-23 19:59:10 +0530

    massge

--- assignment-3/dom.js
+++ assignment-3/dom.js
@@ -1,23 +1,44 @@
-var a = document.getElementById('header')
-a.style.color = "red"
 
-var x = document.getElementById('div1')
-console.log(x)
-x.style.backgroundColor = "green"
-x.style.textDecorationColor = "red"
+function DOM()
+{
+    var demo=document.getElementById("box");
+    demo.style.backgroundColor="lightblue";
+    demo.style.color="white";
+    demo.style.width="48%";
+    demo.style.float="left";
+    
 
 
-var y = document.getElementById('div2')
-console.log(y)
-y.style.backgroundColor = "yellow"
+    var box2=document.getElementById("box1");
+    box2.style.backgroundColor="yellow";
+    box2.style.color="white";
+    box2.style.width="48%";
+    box2.style.float="right";
+    box2.style.margin="auto";
+}
 
-var z = document.getElementById("para1")
-z.style.color = "red"
 
-document.getElementById("div1").style.width = "400px"; 
 
-document.getElementById("div2").style.width = "400px"; 
+// / var a = document.getElementById('header')
+// a.style.color = "red"
 
-document.getElementsById("cont").style.color = "red";
-// var vv = document.getElementsByClassName("second") 
-// vv.style.right = "100px";
+// var x = document.getElementById('div1')
+// console.log(x)
+// x.style.backgroundColor = "green"
+// x.style.textDecorationColor = "red"
+
+
+// var y = document.getElementById('div2')
+// console.log(y)
+// y.style.backgroundColor = "yellow"
+
+// var z = document.getElementById("para1")
+// z.style.color = "red"
+
+// document.getElementById("div1").style.width = "400px"; 
+
+// document.getElementById("div2").style.width = "400px"; 
+
+// document.getElementsById("cont").style.color = "red";
+// // var vv = document.getElementsByClassName("second") 
+// // vv.style.right = "100px";
--- assignment-5/project.js
+++ assignment-5/project.js
@@ -1,18 +1,1 @@
 
-function DOM()
-{
-    var demo = document.getElementById("box");
-    demo.style.backgroundColor="lightblue";
-    demo.style.color="white";
-    demo.style.width="48%";
-    demo.style.float="left";
-    
-
-
-    var box2=document.getElementById("box1");
-    box2.style.backgroundColor="yellow";
-    box2.style.color="white";
-    box2.style.width="48%";
-    box2.style.float="right";
-    box2.style.margin="auto";
-}
