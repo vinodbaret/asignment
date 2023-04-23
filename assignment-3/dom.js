ea6c505f02f3c186961efa805e2ccbebf417490e
vinodbaret <vinodsondliya@gmail.com>
2023-04-23 19:51:05 +0530

    vv

---
+++ assignment-3/dom.js
@@ -0,0 +1,23 @@
+var a = document.getElementById('header')
+a.style.color = "red"
+
+var x = document.getElementById('div1')
+console.log(x)
+x.style.backgroundColor = "green"
+x.style.textDecorationColor = "red"
+
+
+var y = document.getElementById('div2')
+console.log(y)
+y.style.backgroundColor = "yellow"
+
+var z = document.getElementById("para1")
+z.style.color = "red"
+
+document.getElementById("div1").style.width = "400px"; 
+
+document.getElementById("div2").style.width = "400px"; 
+
+document.getElementsById("cont").style.color = "red";
+// var vv = document.getElementsByClassName("second") 
+// vv.style.right = "100px";
